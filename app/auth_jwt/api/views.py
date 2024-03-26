c5a08ab8f6ce8d2dd49ca8b04105e4d4602e34f0
Ivan Barchuninov <bikovshanin@yandex.ru>
2024-03-26 10:38:21 +0300

    Update services and auth_jwt views

--- app/auth_jwt/api/views.py
+++ app/auth_jwt/api/views.py
@@ -1,8 +1,10 @@
+from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
 from app.auth_jwt.api.serializers import TokenOutputSerializer
-from app.core.services import get_user_by_id, get_token
+from app.core.services import get_user_by_id, get_tokens
 
 
 class TokenCreateView(CreateAPIView):
@@ -11,5 +13,12 @@ class TokenCreateView(CreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = TokenOutputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = get_user_by_id(request)
-        return get_token(user)
+        user = get_user_by_id(request.data.get('id'))
+        refresh_token, access_token = get_tokens(user)
+        return Response(
+            {
+                'refresh': str(refresh_token),
+                'access': str(refresh_token)
+            },
+            status=status.HTTP_201_CREATED
+        )
