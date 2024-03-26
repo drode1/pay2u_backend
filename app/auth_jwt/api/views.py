aaf62a91a949beb1a008c363a22a732979b529dd
Egor Remezov <mr.drodel@gmail.com>
2024-03-26 17:11:22 +0300

    formatting, hints

--- app/auth_jwt/api/views.py
+++ app/auth_jwt/api/views.py
@@ -1,24 +1,27 @@
 from rest_framework import status
 from rest_framework.permissions import AllowAny
-from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 
 from app.auth_jwt.api.serializers import TokenOutputSerializer
-from app.core.services import get_user_by_id, get_tokens
+from app.core.api.generics import CreateApiView
+from app.core.services import get_tokens_for_user, get_user_by_id
 
 
-class TokenCreateView(CreateAPIView):
+class TokenCreateView(CreateApiView):
     permission_classes = (AllowAny,)
+    serializer_class = TokenOutputSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer = TokenOutputSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
         user = get_user_by_id(request.data.get('id'))
-        refresh_token, access_token = get_tokens(user)
+        refresh_token, access_token = get_tokens_for_user(user)
+
         return Response(
             {
                 'refresh': str(refresh_token),
-                'access': str(refresh_token)
+                'access': str(access_token)
             },
             status=status.HTTP_201_CREATED
         )
--- app/core/services.py
+++ app/core/services.py
@@ -1,13 +1,13 @@
 from django.shortcuts import get_object_or_404
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, Token
 
 from app.users.models import User
 
 
-def get_user_by_id(user_id: int):
+def get_user_by_id(user_id: int) -> User:
     return get_object_or_404(User, id=user_id)
 
 
-def get_tokens(user: User):
-    token = RefreshToken.for_user(user)
-    return token, token.access_token
+def get_tokens_for_user(user: User) -> tuple[Token, Token]:
+    refresh = RefreshToken.for_user(user)
+    return refresh, refresh.token
