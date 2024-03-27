d2313840299219aed77229e2c28d9acdc9d24e24
Egor Remezov <mr.drodel@gmail.com>
2024-03-28 00:45:56 +0300

    add api (only read) for categories and subscriptions

--- app/subscriptions/api/serializers.py
+++ app/subscriptions/api/serializers.py
@@ -1,9 +1,58 @@
 from rest_framework import serializers
 
-from app.subscriptions.models import Category
+from app.subscriptions.models import Cashback, Category, Subscription, Tariff
 
 
-class CategoryModelSerializer(serializers.ModelSerializer):
+class CategoryReadOutputSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('id', 'name',)
+        fields = (
+            'id',
+            'name',
+        )
+
+
+class CashbackReadOutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cashback
+        fields = (
+            'id',
+            'amount',
+        )
+
+
+class TariffReadOutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tariff
+        fields = (
+            'id',
+            'name',
+            'amount',
+            'description',
+        )
+
+
+class SubscriptionReadOutputSerializer(serializers.ModelSerializer):
+    category = CategoryReadOutputSerializer()
+    cashback = CashbackReadOutputSerializer()
+    tariffs = serializers.SerializerMethodField(
+        method_name='get_tariffs'
+    )
+
+    class Meta:
+        model = Subscription
+        fields = (
+            'id',
+            'name',
+            'image',
+            'description',
+            'is_recommended',
+            'category',
+            'cashback',
+            'tariffs',
+        )
+
+    @staticmethod
+    def get_tariffs(obj):
+        tarrifs = Tariff.objects.filter(subscription_id=obj.id)
+        return TariffReadOutputSerializer(tarrifs, many=True).data
--- config/settings/rest_framework.py
+++ config/settings/rest_framework.py
@@ -14,4 +14,7 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_PAGINATION_CLASS': 'app.core.api.pagination.CustomPagination',
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
 }
