7ece68cd576114efcd94f3a0ebbc06f030cde8b6
Egor Remezov <mr.drodel@gmail.com>
2024-03-28 01:44:17 +0300

    upd full name method

--- app/users/models.py
+++ app/users/models.py
@@ -120,4 +120,4 @@ class User(AbstractUser):
 
     @property
     def full_name(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.first_name} {self.patronymic} {self.last_name}'
